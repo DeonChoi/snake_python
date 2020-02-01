e31f5018c6d9cdd592273f52270f8734ee389fb4
Deon Choi <deon.choi@gmail.com>
2020-01-31 20:59:27 -0800

    Initial commit

---
+++ snake.py
@@ -0,0 +1,10 @@
+import sys
+import pygame
+
+
+pygame.init()
+screen = pygame.display.set_mode((500,500))
+pygame.display.flip()
+pygame.display.set_caption('Snake game by Deon Choi')
+pygame.quit()
+quit()
